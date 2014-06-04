b5e6f5a04417190b42d0c43b4025d90a59b0bb22
Dusty Wilson & Jared M Daugherty <pair+Dusty-Wilson.jmdaugherty@devbootcamp.com>
2014-06-04 11:03:54 -0500

    Migrate user table

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,8 +1,10 @@
 require_relative "../spec_helper"
 
 describe User do
-	it "Should return the user's name" do
-		user = User.create(:name => "Unicorn", :email => "hey@yay.com", :phone_number => "123-456-7890", :password => "hithere")
-		expect(user.name).to eq "Unicorn"
-	end
+  describe "#name" do
+  	it "Should return the user's name" do
+  		user = User.create(:first_name => "Unicorn", :email => "hey@yay.com", :phone_number => "123-456-7890", :password_hash => "hithere")
+  		expect(user.first_name).to eq "Unicorn"
+  	end
+  end
 end
