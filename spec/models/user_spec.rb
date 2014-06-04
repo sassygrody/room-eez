3a184610299eb891eddf02bf440adcea2591487e
Beth & Sasha <pair+cynaria.sassygrody@devbootcamp.com>
2014-06-04 11:32:22 -0500

    added a LET statement, and inefficient email test

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,44 +1,36 @@
 require_relative "../spec_helper"
 
 describe User do
-
-  describe '#first_name' do
-  	it "Should return the user's first name" do
-	  	user = User.create(
-        :first_name => "Unicorn",
+	let(:user) {User.new(:first_name => "Unicorn",
         :last_name => "Magic",
         :email => "hey@yay.com",
         :phone_number => "123-456-7890",
         :password_hash => "hithere"
-      )
+      )}
+
+  describe '#first_name' do
+  	it "Should return the user's first name" do
 	  	expect(user.first_name).to eq "Unicorn"
 	  end
   end
 
   describe '#last_name' do
   	it "Should return the user's last name" do
-	  	user = User.create(
-        :first_name => "Unicorn",
-        :last_name => "Magic",
-        :email => "hey@yay.com",
-        :phone_number => "123-456-7890",
-        :password_hash => "hithere"
-      )
 	  	expect(user.last_name).to eq "Magic"
 	  end
   end
 
   describe "#name" do
   	it "Should return the user's name" do
-  		user = User.create(
-        :first_name => "Unicorn",
-        :last_name => "Magic",
-        :email => "hey@yay.com",
-        :phone_number => "123-456-7890",
-        :password_hash => "hithere"
-      )
   		expect(user.name).to eq "Unicorn Magic"
   	end
   end
-
+ 
+ 	describe '#email' do
+ 		it "Should return the user's email" do
+ 			expect(user.email).to eq "hey@yay.com"
+ 		end
+ 	end
 end
+
+
