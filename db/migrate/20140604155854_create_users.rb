711127c9fc1fd393feba52ec39e7c5960d4142d9
Dusty Wilson <dwwilson21@gmail.com>
2014-06-04 18:00:52 -0500

    deleted duplicate migrations

--- db/migrate/20140604155854_create_users.rb
+++
@@ -1,9 +0,0 @@
-class CreateUsers < ActiveRecord::Migration
-  def change
-    create_table :users do |t|
-        t.string :first_name, :last_name, :email, :password_digest, :phone_number
-
-        t.timestamps
-    end
-  end
-end
