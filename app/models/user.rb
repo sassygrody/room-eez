d98a41b29a04aa75b983d175f233e4e16c49fa31
Dusty Wilson & Jared M Daugherty <pair+Dusty-Wilson.jmdaugherty@devbootcamp.com>
2014-06-04 15:46:47 -0500

    Basic tests for user model complete

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,12 @@
 class User < ActiveRecord::Base
+  has_secure_password
+
+  validates_uniqueness_of :email
+  validates_presence_of :email, :phone_number, :first_name, :last_name
+  validates :email, :format => /\A(\S+)@(.+)\.(\S+)\z/
+  validates :phone_number, :format => /(1-?)?\(?\d{3}[\)|-]\d{3}-\d{4}/
+  validates :password, :length => { :in => 6..20 }
+
 
   def name
     "#{self.first_name} #{self.last_name}"
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,8 +1,12 @@
 ENV["RAILS_ENV"] ||= 'test'
 require File.expand_path("../../config/environment", __FILE__)
 require 'rspec/rails'
+require 'shoulda/matchers'
 require 'rspec/autorun'
 require 'database_cleaner'
+require 'coveralls'
+
+Coveralls.wear!
 
 # Requires supporting ruby files with custom matchers and macros, etc,
 # in spec/support/ and its subdirectories.
